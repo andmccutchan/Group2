0dc8be17db15733d5f8cc35956c41f8e59c514f9
andmccutchan <andmccutchan2004@gmail.com>
2024-11-19 11:38:10 -0500

    font

---
+++ HashMap.cpp
@@ -0,0 +1,21 @@
+#include "HashMap.hpp"
+
+template <class K, class V>
+void HashMap<K, V>::insert(const K &key, const V &value) {
+
+}
+
+template <class K, class V>
+void HashMap<K, V>::remove() {
+
+}
+
+template <class K, class V>
+V& HashMap<K, V>::operator[](const K &key) {
+
+}
+
+template <class K, class V>
+pair<K,V>* HashMap<K, V>::search(const K &key) {
+
+}
