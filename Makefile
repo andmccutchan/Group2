c5f489fb52c52bd29dc1457dc873b798206c4326
Hp0511 <phan_h3@denison.edu>
2024-12-01 15:09:45 -0500

    idk

--- Makefile
+++ Makefile
@@ -1,22 +1,48 @@
+# # Target to build and run the test executable
+# mytests: mytests.o HashMap.o HashFunction.o
+# 	g++ -o mytests mytests.o HashMap.o HashFunction.o
+# 	./mytests
+
+# # Rule for building mytests.o
+# mytests.o: mytests.cpp HashMap.hpp HashFunction.hpp
+# 	g++ -c mytests.cpp -o mytests.o 
+
+# # Rule for building HashMap.o
+# HashMap.o: HashMap.cpp HashMap.hpp HashFunction.hpp
+# 	g++ -c HashMap.cpp -o HashMap.o 
+
+# # Rule for building HashFunction.o
+# HashFunction.o: HashFunction.hpp HashFunction.cpp
+# 	g++ -c HashFunction.cpp -o HashFunction.o 
+
+# # Cleaning all object files and executable
+# clean:
+# 	rm -f *.o mytests
 # Target to build and run the test executable
-mytests: mytests.o HashMap.o HashFunction.o HashMapTree.o
-	g++ -o mytests mytests.o HashMap.o HashFunction.o
+# Target to build and run the test executable
+mytests: mytests.o HashMapTree.o RBTree.o RBTreeNode.o HashFunction.o
+	g++ -o mytests mytests.o HashMapTree.o RBTree.o RBTreeNode.o HashFunction.o
 	./mytests
 
 # Rule for building mytests.o
-mytests.o: mytests.cpp HashMap.hpp HashFunction.hpp HashMapTree.hpp
-	g++ -c mytests.cpp -o mytests.o 
+mytests.o: mytests.cpp HashMapTree.hpp RBTree.hpp RBTreeNode.hpp HashFunction.hpp
+	g++ -c mytests.cpp -o mytests.o
 
-# Rule for building HashMap.o
-HashMap.o: HashMap.cpp HashMap.hpp HashFunction.hpp
-	g++ -c HashMap.cpp -o HashMap.o 
-
-HashMapTree.o: HashMapTree.cpp HashMapTree.hpp HashFunction.hpp	
+# Rule for building HashMapTree.o
+HashMapTree.o: HashMapTree.cpp HashMapTree.hpp RBTree.hpp RBTreeNode.hpp HashFunction.hpp
 	g++ -c HashMapTree.cpp -o HashMapTree.o
 
+# Rule for building RBTree.o
+RBTree.o: RBTree.cpp RBTree.hpp RBTreeNode.hpp
+	g++ -c RBTree.cpp -o RBTree.o
+
+# Rule for building RBTreeNode.o
+RBTreeNode.o: RBTreeNode.cpp RBTreeNode.hpp
+	g++ -c RBTreeNode.cpp -o RBTreeNode.o
+
 # Rule for building HashFunction.o
-HashFunction.o: HashFunction.hpp HashFunction.cpp
-	g++ -c HashFunction.cpp -o HashFunction.o 
+HashFunction.o: HashFunction.cpp HashFunction.hpp
+	g++ -c HashFunction.cpp -o HashFunction.o
 
 # Cleaning all object files and executable
 clean:
--- mytests.cpp
+++ mytests.cpp
@@ -1,147 +1,245 @@
 #include <iostream>
-#include "HashMap.cpp"
-#include "DoublyLinkedList.cpp"
+#include <cassert>
+#include "HashMapTree.hpp"
+// #include "HashMap.cpp"
+// #include "DoublyLinkedList.cpp"
+
+
 using namespace std;
 
-int testsPassed = 0;
-int testsFailed = 0;
+// int testsPassed = 0;
+// int testsFailed = 0;
 
-// Function to handle assertions
-void assertTest(bool condition, const string& testName) {
-    if (condition) {
-        testsPassed++;
-        cout << "[PASS] " << testName << endl;
-    } else {
-        testsFailed++;
-        cout << "[FAIL] " << testName << endl;
+// // Function to handle assertions
+// void assertTest(bool condition, const string& testName) {
+//     if (condition) {
+//         testsPassed++;
+//         cout << "[PASS] " << testName << endl;
+//     } else {
+//         testsFailed++;
+//         cout << "[FAIL] " << testName << endl;
+//     }
+// }
+
+// // Function to print summary of test results
+// void printTestSummary() {
+//     cout << "\n--- Test Summary ---" << endl;
+//     cout << "Tests Passed: " << testsPassed << endl;
+//     cout << "Tests Failed: " << testsFailed << endl;
+//     if (testsFailed == 0) {
+//         cout << "All tests passed!" << endl;
+//     } else {
+//         cout << "Some tests failed. Please review the failed cases." << endl;
+//     }
+// }
+
+// void hashMap_testDefaultConstructor() {
+//     cout << "\nRunning testDefaultConstructor:" << endl;
+//     HashMap<int, int> hashMap;
+
+//     assertTest(hashMap.getSize() == 0, "Default Constructor: Size is 0");
+//     assertTest(hashMap.getCapacity() == 10, "Default Constructor: Capacity is 10");
+
+//     hashMap.print();
+// }
+
+// void hashMap_testCopyConstructor() {
+//     cout << "\nRunning testCopyConstructor:" << endl;
+
+//     HashMap<int, int> hashMap;
+//     hashMap.insert(1, 10);
+//     hashMap.insert(2, 20);
+
+//     HashMap<int, int> copy(hashMap);
+
+//     assertTest(copy.getSize() == 2, "Copy Constructor: Size matches");
+//     assertTest(copy[1] == 10, "Copy Constructor: Key 1 value matches");
+//     assertTest(copy[2] == 20, "Copy Constructor: Key 2 value matches");
+
+//     copy.print();
+// }
+
+// void hashMap_testAssignmentOperator() {
+//     cout << "\nRunning testAssignmentOperator:" << endl;
+
+//     HashMap<int, int> hashMap;
+//     hashMap.insert(1, 10);
+//     hashMap.insert(2, 20);
+
+//     HashMap<int, int> assigned;
+//     assigned = hashMap;
+
+//     assertTest(assigned.getSize() == 2, "Assignment Operator: Size matches");
+//     assertTest(assigned[1] == 10, "Assignment Operator: Key 1 value matches");
+//     assertTest(assigned[2] == 20, "Assignment Operator: Key 2 value matches");
+
+//     assigned.print();
+// }
+
+// void hashMap_testInsert() {
+//     cout << "\nRunning testInsert:" << endl;
+
+//     HashMap<int, int> hashMap;
+//     hashMap.insert(1, 10);
+//     hashMap.insert(2, 20);
+//     hashMap.insert(11, 30); // Collision with 1 if capacity = 10
+
+//     assertTest(hashMap.getSize() == 3, "Insert: Size matches");
+//     assertTest(hashMap[1] == 10, "Insert: Key 1 value matches");
+//     assertTest(hashMap[2] == 20, "Insert: Key 2 value matches");
+//     assertTest(hashMap[11] == 30, "Insert: Key 11 value matches");
+
+//     hashMap.print();
+// }
+
+// void hashMap_testRemove() {
+//     cout << "\nRunning testRemove:" << endl;
+
+//     HashMap<int, int> hashMap;
+//     hashMap.insert(1, 10);
+//     hashMap.insert(2, 20);
+
+//     pair<int, int>* element = hashMap.search(1);
+//     hashMap.remove(element);
+
+//     assertTest(hashMap.getSize() == 1, "Remove: Size decreases");
+//     assertTest(hashMap.search(1) == nullptr, "Remove: Key 1 is removed");
+
+//     hashMap.print();
+// }
+
+// void hashMap_testSearch() {
+//     cout << "\nRunning testSearch:" << endl;
+
+//     HashMap<int, int> hashMap;
+//     hashMap.insert(1, 10);
+//     hashMap.insert(2, 20);
+
+//     pair<int, int>* result = hashMap.search(1);
+//     assertTest(result != nullptr, "Search: Key 1 found");
+//     assertTest(result->second == 10, "Search: Key 1 value matches");
+
+//     result = hashMap.search(3);
+//     assertTest(result == nullptr, "Search: Key 3 not found");
+
+//     hashMap.print();
+// }
+
+// void hashMap_testOperatorAccess() {
+//     cout << "\nRunning testOperatorAccess:" << endl;
+
+//     HashMap<int, int> hashMap;
+//     hashMap[1] = 10;
+//     hashMap[2] = 20;
+
+//     assertTest(hashMap[1] == 10, "Operator[]: Key 1 value matches");
+//     assertTest(hashMap[2] == 20, "Operator[]: Key 2 value matches");
+
+//     hashMap.print();
+// }
+
+void testHashMapTreeInsert() {
+    cout << "Testing HashMapTree insertion" << endl;
+    cout << "=============================" << endl;
+    HashMapTree<int, int> map;
+    map.insert(2,6);
+    map.insert(6,3);
+    map.insert(2,4);
+    map.insert(22, 7);
+    map.insert(1, 14);
+    map.insert(3, 90);
+    map.insert(11, 14);
+    HashMapTree<int, int> map2(map);
+    map.printMap();
+    cout << "=============================" << endl;
+    cout << endl;
+}
+
+void testHashMapTreeRemove() {
+    cout << "Testing HashMapTree removal" << endl;
+    cout << "=============================" << endl;
+    HashMapTree<int, int> map;
+    map.insert(5, 18);
+    map.insert(9, 10);
+    map.insert(7, 2);
+    map.insert(18, 6);
+
+    cout << "BEOFRE: " << endl;
+    map.printMap();
+
+    map.remove(7);
+
+
+    cout << "AFTER: " << endl;
+    map.printMap();
+    cout << "=============================" << endl;
+    cout << endl;
+}
+
+void testHashMapTreeSearch() {
+    cout << "Testing HashMapTree search" << endl;
+    cout << "=============================" << endl;
+    HashMapTree<int, int> map;
+    map.insert(2,6);
+    map.insert(6,3);
+    map.insert(2,4);
+    map.insert(22, 7);
+    map.insert(1, 14);
+    map.insert(3, 90);
+    map.insert(11, 14);
+    map.printMap();
+
+    cout << "Searching for key: 2: ";
+    if (map.search(2) != nullptr) {
+        cout << "Key found!" << endl;
+    } 
+    else {
+        cout << "Key not found :(" << endl;
     }
-}
 
-// Function to print summary of test results
-void printTestSummary() {
-    cout << "\n--- Test Summary ---" << endl;
-    cout << "Tests Passed: " << testsPassed << endl;
-    cout << "Tests Failed: " << testsFailed << endl;
-    if (testsFailed == 0) {
-        cout << "All tests passed!" << endl;
-    } else {
-        cout << "Some tests failed. Please review the failed cases." << endl;
+    cout << "Searching for key: 4: ";
+    if (map.search(4) != nullptr) {
+        cout << "Key found!" << endl;
+    } 
+    else {
+        cout << "Key not found :(" << endl;
     }
+    cout << "=============================" << endl;
+    cout << endl;
 }
 
-void hashMap_testDefaultConstructor() {
-    cout << "\nRunning testDefaultConstructor:" << endl;
-    HashMap<int, int> hashMap;
+void testHashMapTreeAccess() {
+    cout << "Testing HashMapTree search" << endl;
+    cout << "=============================" << endl;
+    HashMapTree<int, int> map;
+    map.insert(2,6);
+    map.insert(6,3);
+    map.insert(2,4);
+    map.insert(22, 7);
+    map.insert(1, 14);
+    map.insert(3, 90);
+    map.insert(11, 14);
+    map.printMap();
 
-    assertTest(hashMap.getSize() == 0, "Default Constructor: Size is 0");
-    assertTest(hashMap.getCapacity() == 10, "Default Constructor: Capacity is 10");
+    cout << "Value with key 2 = " << map[2] << endl;
 
-    hashMap.print();
-}
-
-void hashMap_testCopyConstructor() {
-    cout << "\nRunning testCopyConstructor:" << endl;
-
-    HashMap<int, int> hashMap;
-    hashMap.insert(1, 10);
-    hashMap.insert(2, 20);
-
-    HashMap<int, int> copy(hashMap);
-
-    assertTest(copy.getSize() == 2, "Copy Constructor: Size matches");
-    assertTest(copy[1] == 10, "Copy Constructor: Key 1 value matches");
-    assertTest(copy[2] == 20, "Copy Constructor: Key 2 value matches");
-
-    copy.print();
-}
-
-void hashMap_testAssignmentOperator() {
-    cout << "\nRunning testAssignmentOperator:" << endl;
-
-    HashMap<int, int> hashMap;
-    hashMap.insert(1, 10);
-    hashMap.insert(2, 20);
-
-    HashMap<int, int> assigned;
-    assigned = hashMap;
-
-    assertTest(assigned.getSize() == 2, "Assignment Operator: Size matches");
-    assertTest(assigned[1] == 10, "Assignment Operator: Key 1 value matches");
-    assertTest(assigned[2] == 20, "Assignment Operator: Key 2 value matches");
-
-    assigned.print();
-}
-
-void hashMap_testInsert() {
-    cout << "\nRunning testInsert:" << endl;
-
-    HashMap<int, int> hashMap;
-    hashMap.insert(1, 10);
-    hashMap.insert(2, 20);
-    hashMap.insert(11, 30); // Collision with 1 if capacity = 10
-
-    assertTest(hashMap.getSize() == 3, "Insert: Size matches");
-    assertTest(hashMap[1] == 10, "Insert: Key 1 value matches");
-    assertTest(hashMap[2] == 20, "Insert: Key 2 value matches");
-    assertTest(hashMap[11] == 30, "Insert: Key 11 value matches");
-
-    hashMap.print();
-}
-
-void hashMap_testRemove() {
-    cout << "\nRunning testRemove:" << endl;
-
-    HashMap<int, int> hashMap;
-    hashMap.insert(1, 10);
-    hashMap.insert(2, 20);
-
-    pair<int, int>* element = hashMap.search(1);
-    hashMap.remove(element);
-
-    assertTest(hashMap.getSize() == 1, "Remove: Size decreases");
-    assertTest(hashMap.search(1) == nullptr, "Remove: Key 1 is removed");
-
-    hashMap.print();
-}
-
-void hashMap_testSearch() {
-    cout << "\nRunning testSearch:" << endl;
-
-    HashMap<int, int> hashMap;
-    hashMap.insert(1, 10);
-    hashMap.insert(2, 20);
-
-    pair<int, int>* result = hashMap.search(1);
-    assertTest(result != nullptr, "Search: Key 1 found");
-    assertTest(result->second == 10, "Search: Key 1 value matches");
-
-    result = hashMap.search(3);
-    assertTest(result == nullptr, "Search: Key 3 not found");
-
-    hashMap.print();
-}
-
-void hashMap_testOperatorAccess() {
-    cout << "\nRunning testOperatorAccess:" << endl;
-
-    HashMap<int, int> hashMap;
-    hashMap[1] = 10;
-    hashMap[2] = 20;
-
-    assertTest(hashMap[1] == 10, "Operator[]: Key 1 value matches");
-    assertTest(hashMap[2] == 20, "Operator[]: Key 2 value matches");
-
-    hashMap.print();
+    cout << "=============================" << endl;
+    cout << endl;
 }
 
 int main() {
-    hashMap_testDefaultConstructor();
-    hashMap_testCopyConstructor();
-    hashMap_testAssignmentOperator();
-    hashMap_testInsert();
-    hashMap_testRemove();
-    hashMap_testSearch();
-    hashMap_testOperatorAccess();
-    printTestSummary();
+    // hashMap_testDefaultConstructor();
+    // hashMap_testCopyConstructor();
+    // hashMap_testAssignmentOperator();
+    // hashMap_testInsert();
+    // hashMap_testRemove();
+    // hashMap_testSearch();
+    // hashMap_testOperatorAccess();
+    // printTestSummary();
+
+    testHashMapTreeInsert();
+    testHashMapTreeRemove();
+    testHashMapTreeSearch();
+    testHashMapTreeAccess();
     return 0;
 }
