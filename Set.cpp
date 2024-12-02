825547eda58f9615c26e277c2846c28faf7357d4
namuleekim <kim_n1@denison.edu>
2024-12-01 22:41:00 -0500

    search and test

--- Set.cpp
+++ Set.cpp
@@ -33,7 +33,6 @@ void Set<T>::print() const{
     }
 }
 
-
 template <typename T>
 void Set<T>::insert(const T& value){
     int index = hash.getHash(value);
@@ -62,22 +61,71 @@ void Set<T>::insert(const T& value){
     }
 }
 
+template <typename T>
+void Set<T>::remove(const T& value){
+    int index = hash.getHash(value);
+    auto& chain = HSet[index];
+}
+
+template <typename T>
+bool Set<T>::search(const T& value){
+    int index = hash.getHash(value);
+    auto& chain = HSet[index];
+    if (chain.empty()){
+        return false;
+    }
+    else{
+        for (auto i = chain.begin(); i != chain.end(); i++){
+            if (*i == value){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 
 main(){
-    
+
     Set<int> hset;
     random_device rd;  // Non-deterministic random number generator
     mt19937 gen(rd()); // Mersenne Twister engine
     uniform_int_distribution<int> dist(1, 100); // Distribution for numbers between 1 and 100 (adjust as needed)
 
-    int n = 100;
-    //vector<int> randomNumbers(n);
+    int n = 60;
+    vector<int> randomNumbers(n);
+    vector<int> wrongNumbers(n);
     for (int i = 0; i < n; ++i) {
-        //randomNumbers[i] = dist(gen);
-        hset.insert(dist(gen));
+        randomNumbers[i] = dist(gen);
+        hset.insert(randomNumbers[i]);
+    }
+
+    hset.print();
+
+    for (int i = 0; i < n; i++){
+        //n = dist(gen);
+        wrongNumbers[i] = randomNumbers[i]*-1;
+        n = randomNumbers[i];
+        cout << "Searching for " << n << "... ";
+        if(hset.search(n)){
+            cout << "FOUND" << endl;
+        }
+        else{
+            cout << "NOT_FOUND" << endl;
+        }
+    }
+
+        for (int i = 0; i < n; i++){
+        //n = dist(gen);
+        int m = wrongNumbers[i];
+        cout << "Searching for " << m << "... ";
+        if(hset.search(m)){
+            cout << "FOUND" << endl;
+        }
+        else{
+            cout << "NOT_FOUND" << endl;
+        }
     }
 
 
-
-    hset.print();
 }
